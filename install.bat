ab3e0c7dac14ceef7b73cda0e0f00f1d731db5bf
azkhanhkj <azkhanhkj@gmail.com>
2026-08-17 08:26:44 +0000

    update

--- install.bat
+++ install.bat
@@ -11,8 +11,11 @@ if /I not "%USERNAME%"=="%TARGET_USER%" (
 
 choco feature enable -n allowGlobalConfirmation
 
+call recaf.bat
+
 choco install intellijidea-community --version 2024.1.5 --yes
 choco install opencode github-desktop temurin17 temurin8 ghidra --yes
+choco uninstall apache-httpd aria2 hg Minikube nginx nssm packer rtools sbt strawberryperl swig tortoisesvn unzip wixtoolset julia R.Project php composer pulumi bicep bazel azcopy10 awscli kubernetes-cli kubernetes-helm gradle imagemagick imagemagick.app --yes --no-progress
 
 :: Install Bun using the official installer
 powershell -NoProfile -ExecutionPolicy Bypass -Command "irm https://bun.sh/install.ps1 | iex"
