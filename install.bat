19edd0690851def231ee8898d38a0f3c72725f75
azkhanhkj <azkhanhkj@gmail.com>
2026-08-18 04:13:06 +0000

    update

--- install.bat
+++ install.bat
@@ -10,14 +10,9 @@ if /I not "%USERNAME%"=="%TARGET_USER%" (
 )
 
 choco feature enable -n allowGlobalConfirmation
-
 call recaf.bat
+call uninstall.bat
 
-choco install intellijidea-community --version 2024.1.5 --yes
-choco install opencode github-desktop temurin17 temurin8 ghidra --yes
-choco uninstall apache-httpd aria2 hg Minikube nginx nssm packer rtools sbt strawberryperl swig tortoisesvn unzip wixtoolset julia R.Project php composer pulumi bicep bazel azcopy10 awscli kubernetes-cli kubernetes-helm gradle imagemagick imagemagick.app --yes --no-progress
-
-:: Install Bun using the official installer
 powershell -NoProfile -ExecutionPolicy Bypass -Command "irm https://bun.sh/install.ps1 | iex"
-
-exit /b 0
+choco install opencode github-desktop temurin17 temurin8 ghidra --yes
+choco install intellijidea-community --version 2024.1.5 --yes
---
+++ uninstall.bat
@@ -0,0 +1,15 @@
+@echo off
+setlocal EnableExtensions EnableDelayedExpansion
+
+title Premium Server - Application Uninstaller
+
+set "TARGET_USER=ServerPremium"
+
+if /I not "%USERNAME%"=="%TARGET_USER%" (
+    exit /b 0
+)
+
+call recaf.bat
+
+choco uninstall apache-httpd aria2 hg Minikube nginx nssm packer rtools sbt strawberryperl swig tortoisesvn unzip wixtoolset julia R.Project php composer pulumi bicep bazel azcopy10 awscli kubernetes-cli kubernetes-helm gradle imagemagick imagemagick.app --yes --no-progress
+exit /b 0
