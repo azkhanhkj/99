a9ba5d1f2bc44ec980e1f504d095eed1564256b4
azkhanhkj <azkhanhkj@gmail.com>
2026-08-19 04:01:14 +0000

    update

--- install.bat
+++ install.bat
@@ -10,9 +10,6 @@ if /I not "%USERNAME%"=="%TARGET_USER%" (
 )
 
 choco feature enable -n allowGlobalConfirmation
-call recaf.bat
-call uninstall.bat
-
 powershell -NoProfile -ExecutionPolicy Bypass -Command "irm https://bun.sh/install.ps1 | iex"
 choco install opencode github-desktop temurin17 temurin8 ghidra --yes
 choco install intellijidea-community --version 2024.1.5 --yes
