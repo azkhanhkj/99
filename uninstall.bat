c2e382fa310dc53f0e088d80147b6c9faaa17c61
azkhanhkj <azkhanhkj@gmail.com>
2026-08-21 10:16:51 +0000

    update

--- uninstall.bat
+++ uninstall.bat
@@ -9,7 +9,5 @@ if /I not "%USERNAME%"=="%TARGET_USER%" (
     exit /b 0
 )
 
-call recaf.bat
-
 choco uninstall apache-httpd aria2 hg Minikube nginx nssm packer rtools sbt strawberryperl swig tortoisesvn unzip wixtoolset julia R.Project php composer pulumi bicep bazel azcopy10 awscli kubernetes-cli kubernetes-helm gradle imagemagick imagemagick.app --yes --no-progress
 exit /b 0
