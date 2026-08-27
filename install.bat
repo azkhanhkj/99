322b90a40366ade99b9a7460808c2efd52ec1ec7
azkhanhkj <azkhanhkj@gmail.com>
2026-08-27 03:57:19 +0000

    update

--- install.bat
+++ install.bat
@@ -12,8 +12,6 @@ if /I not "%USERNAME%"=="%TARGET_USER%" (
 choco feature enable -n allowGlobalConfirmation
 powershell -NoProfile -ExecutionPolicy Bypass -Command "irm https://bun.sh/install.ps1 | iex"
 call refreshenv
+choco install intellijidea-community github-desktop antigravity-ide temurin17 temurin8 vscode ghidra --yes
 powershell -NoProfile -ExecutionPolicy Bypass -Command "irm https://omp.sh/install.ps1 | iex"
-choco install antigravity-ide github-desktop temurin17 temurin8 vscode ghidra --yes
-choco install intellijidea-community --version 2024.1.5 --yes
-
 bun add -g opencode-ai
